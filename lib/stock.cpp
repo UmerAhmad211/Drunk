c432f0dc0793e2e2ae7d6bac65ac0acacaf43afb
umer <bscs23211@itu.edu.pk>
2024-10-01 14:59:24 +0500

    feature: add mouse click for stock

--- lib/stock.cpp
+++ lib/stock.cpp
@@ -1,11 +1,46 @@
 #include "stock.hpp"
 #include <raylib.h>
+#include <iostream>
+#define CORD_X 64
+#define CORD_Y 29
+#define S_SIZE 23
 
-stock::stock(init* cards_inst) : cards(cards_inst) {}
+stock::stock(init* cards_inst) : init_cards(cards_inst) {
+  for (size_t i = 0; i <= S_SIZE; ++i)
+    cards_stock.push(init_cards->deck[i]);
+  if (cards_stock.empty())
+    std::cerr << "Empty." << std::endl;
+  spacing = 150;
+}
+// delete
 stock::~stock() {}
 
 void stock::draw_stock() {
-  const int cord_x = 64;
-  const int cord_y = 29;
-  DrawTexture(cards->card_back, cord_x, cord_y, WHITE);
+  DrawTexture(init_cards->card_back, CORD_X, CORD_Y, WHITE);
+  if (!waste.empty())
+    DrawTexture(waste.top().cards, CORD_X + spacing, CORD_Y, WHITE);
+}
+
+void stock::stock_clicked(Vector2 mouse_pos) {
+  if ((CheckCollisionPointRec(mouse_pos,
+                              (Rectangle){CORD_X, CORD_Y, T_WIDTH, T_HEIGHT})))
+    move_to_waste();
+}
+
+void stock::move_to_waste() {
+  if (!cards_stock.empty()) {
+    cards_stock.top().isnt_hidden = true;
+    waste.push(cards_stock.top());
+    cards_stock.pop();
+  } else
+    restock();
+}
+
+void stock::restock() {
+  for (; !waste.empty();) {
+    waste.top().isnt_hidden = false;
+    cards_stock.push(waste.top());
+    waste.pop();
+  }
+  draw_stock();
 }
--- lib/stock.hpp
+++ lib/stock.hpp
@@ -1,14 +1,22 @@
 #ifndef STOCK_HPP
 #define STOCK_HPP
+#include <stack>
 #include "init.hpp"
+#include "raylib.h"
 
 class stock {
-  init* cards;
+  init* init_cards;
+  unsigned int spacing;
+  std::stack<cards_props> cards_stock;
+  std::stack<cards_props> waste;
 
  public:
   stock(init*);
   ~stock();
   void draw_stock();
+  void move_to_waste();
+  void restock();
+  void stock_clicked(Vector2);
 };
 
 #endif
