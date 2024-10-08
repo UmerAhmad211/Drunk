b2ae5c5242e2f8167638df1c42f676a00fd0e813
umer <bscs23211@itu.edu.pk>
2024-10-08 14:23:51 +0500

    feat: add card movement

--- lib/tableau.cpp
+++ lib/tableau.cpp
@@ -4,29 +4,28 @@
 #include <cstring>
 #include "init.hpp"
 
-tableau::tableau(init* card_inst) : cards(card_inst) {}
+tableau::tableau(init* card_inst) : cards(card_inst) {
+  is_first = true;
+}
 tableau::~tableau() {
   delete cards;
 };
 
 void tableau::draw_tableau() {
-  int x = 150, spacing = 160;
-  int y = 258;
-  for (size_t i = 0; i < 7; ++i) {
+  for (size_t i = 0; i < COL_NUM; ++i) {
     for (size_t j = 0; j < tableau_seven[i].size(); ++j) {
       if (tableau_seven[i][j].isnt_hidden)
-        DrawTexture(tableau_seven[i][j].cards, x, y, WHITE);
+        DrawTexture(tableau_seven[i][j].cards, tableau_seven[i][j].position.x,
+                    tableau_seven[i][j].position.y, WHITE);
       else
-        DrawTexture(cards->card_back, x, y, WHITE);
-      y += 27;
+        DrawTexture(cards->card_back, tableau_seven[i][j].position.x,
+                    tableau_seven[i][j].position.y, WHITE);
     }
-    x += spacing;
-    y = 258;
   }
 }
 
 void tableau::tableau_init() {
-  int card_col = 7, j = 0;
+  int j = 0;
   for (size_t i = 24; i < CARDS_NO; ++i) {
     tableau_seven[j].push_back(cards->deck[i]);
     if (i == 24 || i == 26 || i == 29 || i == 33 || i == 38 || i == 44 ||
@@ -35,6 +34,59 @@ void tableau::tableau_init() {
   }
 }
 
-// void tableau::tableau_move(){
-//
-// }
+void tableau::tableau_move(Vector2 mouse_pos) {
+  if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+    for (size_t i = 0; i < COL_NUM; ++i) {
+      for (size_t j = 0; j < tableau_seven[i].size(); ++j) {
+        Rectangle card_rect = {
+            tableau_seven[i][j].position.x, tableau_seven[i][j].position.y,
+            static_cast<float>(tableau_seven[i][j].cards.width),
+            static_cast<float>(tableau_seven[i][j].cards.height)};
+        if (CheckCollisionPointRec(mouse_pos, card_rect) &&
+            tableau_seven[i][j].isnt_hidden) {
+          dragging = true;
+          i_x = i;
+          i_y = j;
+          offset.x = mouse_pos.x - tableau_seven[i][j].position.x;
+          offset.y = mouse_pos.y - tableau_seven[i][j].position.y;
+          return;
+        }
+      }
+    }
+  } else if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
+    dragging = false;
+  }
+
+  if (dragging) {
+    Vector2 nmouse_pos = GetMousePosition();
+    tableau_seven[i_x][i_y].position.x = nmouse_pos.x - offset.x;
+    tableau_seven[i_x][i_y].position.y = nmouse_pos.y - offset.y;
+  }
+}
+void tableau::is_valid_move(fndtion& n_fnd) {
+  Vector2 nmouse_pos = GetMousePosition();
+  if (dragging) {
+    Rectangle card_rect = {
+        tableau_seven[i_x][i_y].position.x, tableau_seven[i_x][i_y].position.y,
+        static_cast<float>(tableau_seven[i_x][i_y].cards.width),
+        static_cast<float>(tableau_seven[i_x][i_y].cards.height)};
+    Rectangle fnd_rect = set_rect(tableau_seven[i_x][i_y].card_type, n_fnd);
+    Rectangle intersect_rect = GetCollisionRec(card_rect, fnd_rect);
+    Rectangle cursor_box = {nmouse_pos.x - 3, nmouse_pos.y - 3, 3, 3};
+    float intersect_area = intersect_rect.width * intersect_rect.height;
+    float cursor_area = cursor_box.width * cursor_box.height;
+    if (intersect_area / cursor_area >= CAPT_THRES) {
+      tableau_seven[i_x][i_y].position.x =
+          fnd_rect.x +
+          (fnd_rect.width - tableau_seven[i_x][i_y].cards.width) / 2;
+      tableau_seven[i_x][i_y].position.y =
+          fnd_rect.y +
+          (fnd_rect.height - tableau_seven[i_x][i_y].cards.height) / 2;
+      n_fnd.update_fnd(tableau_seven[i_x][i_y]);
+      tableau_seven[i_x].pop_back();
+      dragging = false;
+      if (!tableau_seven[i_x].empty())
+        tableau_seven[i_x].back().isnt_hidden = true;
+    }
+  }
+}
--- lib/tableau.hpp
+++ lib/tableau.hpp
@@ -1,19 +1,31 @@
 #ifndef TABLEAU_HPP
 #define TABLEAU_HPP
-#include <raylib.h>
+
 #include <vector>
+#include "fndtion.hpp"
 #include "init.hpp"
+#include "raylib.h"
+
+#define COL_NUM 7
+#define CAPT_THRES 0.5f
 
 class tableau {
   init* cards;
-  std::vector<cards_props> tableau_seven[7];
+  std::vector<cards_props> tableau_seven[COL_NUM];
   bool is_first;
-  // bool drag = false, captured = false;
-  // int x = 630, y = 29, spacing = 160, width = 127, height = 172;
-  // Rectangle fnd_recs[4] = {{x, y, width, height},
-  //                          {x + spacing, y, width, height},
-  //                          {x + (spacing * 2), y, width, height},
-  //                          {x + (spacing * 3), y, width, height}};
+  bool dragging = false;
+  size_t i_x = 0, i_y = 0;
+  Vector2 offset = {0, 0};
+  Rectangle set_rect(std::string card_type, fndtion n_fnd) {
+    if (card_type == "assets/heart")
+      return n_fnd.fnd_rec1;
+    else if (card_type == "assets/diams")
+      return n_fnd.fnd_rec2;
+    else if (card_type == "assets/clubs")
+      return n_fnd.fnd_rec3;
+    else
+      return n_fnd.fnd_rec4;
+  }
 
  public:
   tableau(init*);
@@ -22,6 +34,7 @@ class tableau {
   void tableau_init();
   void update_tableau();
   void tableau_move(Vector2);
+  void is_valid_move(fndtion&);
 };
 
 #endif
