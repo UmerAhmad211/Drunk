ad00d68522b1d2b566b15df19969d01f80c32feb
umer <bscs23211@itu.edu.pk>
2024-10-08 14:22:14 +0500

    feat: add foundation logic

--- lib/fndtion.cpp
+++ lib/fndtion.cpp
@@ -2,19 +2,31 @@
 #include <raylib.h>
 #include <cstddef>
 
-fndtion::fndtion(init* cards_inst) : fnd_track(cards_inst) {
-  spacing = 0;
-}
+fndtion::fndtion() {};
 fndtion::~fndtion() {
-  delete fnd_track;
+  for (size_t i = 0; i < FND_NO; ++i) {
+    for (size_t j = 0; j < fnd_four[i].size(); ++j)
+      UnloadTexture(fnd_four[i][j].cards);
+  }
 }
 
 void fndtion::draw_fnd() {
   for (size_t i = 0; i < FND_NO; ++i) {
-    for (size_t j = 0; j < fnd_four[i].size(); ++j)
-      DrawTexture(fnd_four[i][j].cards, FND_X + spacing, FND_Y, WHITE);
-
-    spacing += 150;
-    spacing %= 600;
+    for (size_t j = 0; j < fnd_four[i].size(); ++j) {
+      if (fnd_four[i][j].isnt_hidden)
+        DrawTexture(fnd_four[i][j].cards, fnd_four[i][j].position.x,
+                    fnd_four[i][j].position.y, WHITE);
+    }
   }
+}
+
+void fndtion::update_fnd(cards_props cards) {
+  if (cards.card_type == "assets/heart")
+    fnd_four[0].push_back(cards);
+  else if (cards.card_type == "assets/diams")
+    fnd_four[1].push_back(cards);
+  else if (cards.card_type == "assets/clubs")
+    fnd_four[2].push_back(cards);
+  else
+    fnd_four[3].push_back(cards);
 }
--- lib/fndtion.hpp
+++ lib/fndtion.hpp
@@ -1,5 +1,6 @@
 #ifndef FNDTION_HPP
 #define FNDTION_HPP
+#include <raylib.h>
 #include <vector>
 #include "init.hpp"
 
@@ -8,15 +9,17 @@
 #define FND_Y 29
 
 class fndtion {
-  init* fnd_track;
-  std::vector<cards_props> fnd_four[FND_NO]{};
-  int spacing;
+  std::vector<cards_props> fnd_four[FND_NO];
 
  public:
-  fndtion(init*);
+  Rectangle fnd_rec1 = {FND_X, FND_Y, T_WIDTH, T_HEIGHT};
+  Rectangle fnd_rec2 = {FND_X + 160, FND_Y, T_WIDTH, T_HEIGHT};
+  Rectangle fnd_rec3 = {FND_X + (2 * 160), FND_Y, T_WIDTH, T_HEIGHT};
+  Rectangle fnd_rec4 = {FND_X + (3 * 160), FND_Y, T_WIDTH, T_HEIGHT};
+  fndtion();
   ~fndtion();
   void draw_fnd();
-  void update_fnd();
+  void update_fnd(cards_props);
 };
 
 #endif
